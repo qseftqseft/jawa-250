ccd1c22f21f884df983dcdc35f2d356451049d9b
qseft <qseft@qseftweb.wz.cz>
2026-09-03 10:33:39 +0200

    hodx

---
+++ Calc.java
@@ -0,0 +1,6 @@
+public class Calc {
+    public String inside = "eins";
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        System.out.println(Calc.add(10, 10));
+        Calc calc = new Calc();
+        System.out.println(calc.inside);
+        
+    }
 }
